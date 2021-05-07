7d49c68e01750d7e04b0fa3fa373aeae92c7ea3c
youssefshaban <youssef.shaban.atta@gmail.com>
2021-05-07 14:46:00 +0200

    lab1

---
+++ complex.rb
@@ -0,0 +1,66 @@
+class ComplexCalc
+    @@addoperation = 0
+    @@multiplyoperation = 0
+    attr_accessor :realNumber, :imgNumber
+
+    def initialize(realNumber, imgNumber)
+      @realNumber = realNumber
+      @imgNumber = imgNumber
+    end
+
+    def + (comblex2)
+      @@addoperation += 1
+      realNumber = @realNumber + comblex2.realNumber
+      imgNumber  = @imgNumber  + comblex2.imgNumber
+      ComplexCalc.new(realNumber, imgNumber)
+    end
+
+    def *(other)
+      @@multiplyoperation += 1
+      realNumber = @realNumber * other.realNumber - @imgNumber * other.imgNumber
+      imgNumber  = @realNumber * other.imgNumber + @imgNumber * other.realNumber
+      ComplexCalc.new(realNumber, imgNumber)
+    end
+
+    def self.bulk_add(listOfComblexs)
+      result = listOfComblexs.first
+      listOfComblexs.drop(1).each do |listOfComblexs|
+        result += listOfComblexs
+      end
+      result
+    end
+
+    def self.bulk_multiply(listOfComblexs)
+      result = listOfComblexs.first
+      listOfComblexs.drop(1).each do |listOfComblexs|
+        result *= listOfComblexs
+      end
+      result
+    end
+
+    
+    def to_s
+      "(#{self.realNumber} + #{self.imgNumber}i)"
+    end
+
+    def self.get_state()
+      puts "No. Add Operation: #{@@addoperation}"
+      puts "No. Multiply Operation: #{@@multiplyoperation}"
+    end
+  
+  end
+  
+  complexnumber1 = ComplexCalc.new(5, 6)
+  complexnumber2 = ComplexCalc.new(3, 9)
+  
+  puts complexnumber1 + complexnumber2
+  puts complexnumber1 * complexnumber2
+  
+  #array operation
+  arr = [complexnumber1, complexnumber2, complexnumber1]
+  
+  puts ComplexCalc.bulk_add(arr)
+  puts ComplexCalc.bulk_multiply(arr)
+  
+  #get state
+  ComplexCalc.get_state()
